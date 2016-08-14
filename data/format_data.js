e53ed159abbb51fd9e471222e344410e607ec12e
AlTavares <alexandre@live.in>
2016-08-14 18:43:21 -0300

    Formatted resume data

--- data/format_data.js
+++ data/format_data.js
@@ -3,6 +3,7 @@ let fs = require('fs')
 let data = []
 data = require('./dados.v1.json')
 let allRefs = new Set()
+let print = console.log
 
 function Reference() {
     this.name = ''
@@ -40,6 +41,32 @@ function extractReferences(element) {
     return { text: text, refs: refs }
 }
 
+/**
+ * @param {string} element
+ */
+function extractMedicines(element) {
+    let index = element.lastIndexOf('(')
+    if (index == -1) {
+        return { text: element.trim(), medicines: [] }
+    }
+    let text = element.substring(0, index)
+    element = element.substring(index).replace(/,/gi, ';').replace(/\(|\)|\./gi, '')
+    let medicines = element.split(';').map(toTitleCase)
+    print(medicines)
+    return { text: text.trim(), medicines: medicines }
+}
+
+function toTitleCase(str, separator) {
+    if (separator == undefined) {
+        separator = ' '
+    }
+    return str.toLowerCase()
+        .trim()
+        .split(separator)
+        .map(i => i[0].toUpperCase() + i.substring(1))
+        .join(separator)
+}
+
 function trimAndAddToSet(string) {
     let s = string.replace(/[()]/g, '').trim()
     allRefs.add(s)
@@ -52,10 +79,26 @@ function saveRefs() {
     fs.writeFileSync('references.json', output)
 }
 
+function formatRemume() {
+    let data = []
+    data = require('./remume.json')
+    for (var index = 0; index < data.length; index++) {
+        let el = data[index]
+        let meds = extractMedicines(el.description)
+        el.medicines = meds.medicines
+        el.description = meds.text
+        // print(el)
+    }
+    let output = JSON.stringify(data, null, 4)
+    fs.writeFileSync('remumev2.json', output)
+
+}
+
 function run() {
     console.log('Formatting...')
-    format()
-    saveRefs()
+    // format()
+    // saveRefs()
+    formatRemume()
     console.log('Finished!')
 }
 
