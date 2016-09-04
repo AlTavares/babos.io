0b291592f5e4d5080f1b554ede15b9cb59fc46f0
AlTavares <alexandre@live.in>
2016-09-04 16:33:38 -0300

    Insert Remume list on DB

--- data/insert-data-in-database.js
+++ data/insert-data-in-database.js
@@ -3,28 +3,37 @@ var env = require('../Server/ParseServer/server-config.json').apps[0].env
 var Parse = require('parse/node')
 Parse.initialize(env.appId)
 Parse.serverURL = 'http://localhost:3000/parse'
+var print = console.log
 
-var data = require('./interactionsLoc.json')
+var interactions = require('./interactionsLoc.json')
 var Interaction = Parse.Object.extend("Interaction");
+saveData(interactions, Interaction)
 
-var interactions = []
-for (var index = 0; index < data.length; index++) {
-    var element = data[index];
-    var interaction = new Interaction();
-    for (let prop in element) {
-        interaction.set(prop, element[prop])
-        interactions.push(interaction)
+var remume = require('./remume.json')
+var Remume = Parse.Object.extend("Remume")
+saveData(remume, Remume)
+
+
+function saveData(data, PFObject) {
+    let list = []
+    for (var index = 0; index < data.length; index++) {
+        var element = data[index];
+        var pfObject = new PFObject();
+        for (let prop in element) {
+            pfObject.set(prop, element[prop])
+            list.push(pfObject)
+        }
     }
+    print(list)
+    Parse.Object.saveAll(list, {
+        success: function (objs) {
+            console.log(objs.length + ' objects saved')
+            // objects have been saved...
+        },
+        error: function (error) {
+            console.log(error)
+            // an error occurred...
+
+        }
+    })
 }
-
-Parse.Object.saveAll(interactions, {
-    success: function (objs) {
-        console.log(objs.length + ' objects saved')
-        // objects have been saved...
-    },
-    error: function (error) {
-        console.log(error)
-        // an error occurred...
-
-    }
-})
