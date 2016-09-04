452f51ade38906113511002f623d64106c7be13d
AlTavares <alexandre@live.in>
2016-09-04 19:16:33 -0300

    Ignoring private config file

---
+++ app/babos.io/babos.io/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  babos.io
+//
+//  Created by Alexandre Mantovani Tavares on 9/4/16.
+//  Copyright © 2016 Alexandre Mantovani Tavares. All rights reserved.
+//
+
+import Foundation
+
+
+struct Environment {
+    
+}
