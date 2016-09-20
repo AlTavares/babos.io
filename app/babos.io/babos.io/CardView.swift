c96912288181ff18c117aa702203cbf420ebefd9
AlTavares <alexandre@live.in>
2016-09-20 20:39:08 -0300

    Added CardView

---
+++ app/babos.io/babos.io/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  babos.io
+//
+//  Created by Alexandre Mantovani Tavares on 9/20/16.
+//  Copyright © 2016 Alexandre Mantovani Tavares. All rights reserved.
+//
+
+import UIKit
+import NibDesignable
+
+class CardView: NibDesignable {
+
+   
+    override func prepareForInterfaceBuilder() {
+        
+    }
+ 
+
+}
