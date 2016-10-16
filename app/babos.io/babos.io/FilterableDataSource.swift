8ee35b0d22b4bf64364770eeec0e4498e8b3cb3a
AlTavares <alexandre@live.in>
2016-10-16 16:11:31 -0200

    Create FilterableDataSource

---
+++ app/babos.io/babos.io/FilterableDataSource.swift
@@ -0,0 +1,32 @@
+//
+//  FilterableDataSource.swift
+//  babos.io
+//
+//  Created by Alexandre Mantovani Tavares on 16/10/16.
+//  Copyright © 2016 Alexandre Mantovani Tavares. All rights reserved.
+//
+
+import Foundation
+
+struct FilterableDataSource<DataType> {
+    var filtered = false
+    var filter: ((DataType) -> Bool)?
+    private var filteredDataSource = [DataType]()
+    private var fullDataSource = [DataType]()
+    
+    var dataSource: [DataType] {
+        get {
+            if (filtered && filter != nil) {
+                return filteredDataSource
+            }
+            return fullDataSource
+        }
+        set {
+            fullDataSource = newValue
+            if let filter = filter {
+                filteredDataSource = newValue.filter(filter)
+            }
+        }
+    }
+
+}
